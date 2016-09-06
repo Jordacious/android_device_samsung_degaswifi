1449cfc07f9540a09db77da24dec1f9154e69357
CTXz <ctx.xda@gmail.com>
2016-09-06 17:58:43 +0200

    bootanim: Define screen size
    Signed-off-by: CTXz <ctx.xda@gmail.com>

--- device.mk
+++ device.mk
@@ -12,4 +12,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#	
+#
+
+LOCAL_PATH := $(call my-dir)
+
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 800
