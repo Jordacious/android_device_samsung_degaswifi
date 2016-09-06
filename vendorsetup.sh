f0ee14459d140f1e0230bda48d0af72a7458711c
CTXz <ctx.xda@gmail.com>
2016-09-06 12:27:57 +0200

    cm: Get proprietary packages if not present
    vendor/cm/common.mk requires build targets located in the the proprietary CM
    packages, they will be downloaded on envsetup.sh if they aren't already present
    
    Signed-off-by: CTXz <ctx.xda@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,7 @@
+# Get proprietary prebuilts it they aren't present yet
+if [ ! -d vendor/cm/prebuilt ]; then
+	sh vendor/cm/get-prebuilts
+fi
+
 add_lunch_combo cm_degaswifi-userdebug
 add_lunch_combo cm_degaswifi-eng
